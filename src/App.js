d1e07d9671b2e1ae9f68d19828c6592e3735fa19
Fetra Andria <f.andriamahaly@gmail.com>
2021-06-07 16:51:44 +0200

    créations Pages, components, styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import Products from './Pages/Products';
+import Services from './Pages/Services';
+import SignUp from './Pages/SignUp';
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Odyssey</h1>
-    </div>
-  );
+  return(
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/services' component={Services} />
+        <Route path='/products' component={Products} />
+        <Route path='/sign-up' component={SignUp} />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
